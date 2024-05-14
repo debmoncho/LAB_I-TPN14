3b910614e87cb3828248d6af8fe50863e80854dd
debmoncho <elmetalerodelsur@gmail.com>
2024-05-14 10:15:05 -0300

    Update LAB_ITPN14.java

--- src/lab_i/tpn14/LAB_ITPN14.java
+++ src/lab_i/tpn14/LAB_ITPN14.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package lab_i.tpn14;
 
 /**
@@ -10,11 +7,12 @@ package lab_i.tpn14;
  */
 public class LAB_ITPN14 {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        //probando probando
+        
+        
     }
     
 }
