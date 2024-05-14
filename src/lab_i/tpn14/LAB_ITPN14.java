fd70d4b93650fed070ab0c1f0f032cda5cd76083
debmoncho <elmetalerodelsur@gmail.com>
2024-05-14 10:14:08 -0300

    Initial commit

---
+++ src/lab_i/tpn14/LAB_ITPN14.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab_i.tpn14;
+
+/**
+ *
+ * @author Deb APBT
+ */
+public class LAB_ITPN14 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
