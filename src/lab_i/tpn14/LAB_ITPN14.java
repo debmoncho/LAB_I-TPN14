e21a0a39064c859ecd4fea3fd4b8a7834137c40b
EstebanSalinas50 <estebankun31@gmail.com>
2024-05-16 17:04:47 -0300

    cambios

--- src/lab_i/tpn14/LAB_ITPN14.java
+++ src/lab_i/tpn14/LAB_ITPN14.java
@@ -1,4 +1,3 @@
-
 package lab_i.tpn14;
 
 import AccesoADatos.AlumnoData;
@@ -7,22 +6,48 @@ import entidades.Alumno;
 import java.sql.*;
 import java.time.LocalDate;
 
-
 public class LAB_ITPN14 {
 
-
     public static void main(String[] args) {
-        
-        
+
         Connection con = Conexion.getConexion();
-        
+
         // PROBANDO CAMBIO A VER
-        
-        Alumno pedro = new Alumno(5,40567231, "Luna", "Juan Pedro", LocalDate.of(1997,11,21), true);
-        AlumnoData data = new AlumnoData();
+//        Alumno pedro = new Alumno(5, 40567231, "Luna", "Juan Pedro", LocalDate.of(1997, 11, 21), true);
+//        AlumnoData data = new AlumnoData();
 //        data.guardarAlumno(pedro);
 //         data.modificarAlumno(pedro);      
-            data.eliminarAlumno(5);
+//            data.eliminarAlumno(5);
+
+// BUSCAR ALUMNO POR ID
+//        Alumno encontrado= data.buscarAlumno(4);
+//        
+//        System.out.println("DNI "+encontrado.getDni());
+//        System.out.println("Apellido "+encontrado.getApellido());
+//        System.out.println("Nombre "+encontrado.getNombre());
+//        
+//        Alumno encontrado = data.buscarAlumnoPorDni(31594876);
+//
+//        if (encontrado != null) {
+//            System.out.println("DNI " + encontrado.getDni());
+//            System.out.println("Apellido " + encontrado.getApellido());
+//            System.out.println("Nombre " + encontrado.getNombre());
+//        }
+        AlumnoData data = new AlumnoData();
+        
+        for(Alumno alumno:data.listarAlumno()){
+            //manera ordenada de ver los datos de los elementos
+//            System.out.println(alumno.getDni());
+//            System.out.println(alumno.getApellido());
+//            System.out.println(alumno.getNombre());
+//            System.out.println(alumno.getFechaNac());
+//            System.out.println("");
+//      ver los elementos con un to string
+            System.out.println(alumno);
+            System.out.println("");
+        }
+        
+
     }
-    
+
 }
